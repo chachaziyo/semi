f9044344abb6f4ae0fdd7d3af789974e455f94ca
user <user@chacha>
2022-11-17 09:51:12 +0900

    12라인 변수추가 후 수정

--- gittest/src/com/git/run/Main.java
+++ gittest/src/com/git/run/Main.java
@@ -9,6 +9,9 @@ public class Main {
 		String test = "유병승";
 		System.out.println(a+test);//commit해야만 저장된다!->gift Staging!에서 commit하기!
 		
+		String title = "내용";
+		System.out.println(title + "이건 어떻게 수정?");
+		
 	}
 
 }
